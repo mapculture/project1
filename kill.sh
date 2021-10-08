0fb930ae0d9b4a2ee60db2270c20fa53343012ae
Kelemen Szimonisz <kelemen.szimonisz@gmail.com>
2021-10-07 18:46:52 -0700

    added docker-compose. updated run/kill scripts

--- kill.sh
+++ kill.sh
@@ -1,2 +1,6 @@
-sudo docker kill p1_rough
-sudo docker rm p1_rough
+# Kill container (without docker compose)
+#sudo docker kill p1_rough
+#sudo docker rm p1_rough
+
+# Stop container (with docker compose)
+sudo docker-compose down
--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
-sudo docker build -t project1_rough:latest .
-sudo docker run -it -d -p 5000:5000 --name p1_rough project1_rough
+# Run script without docker-compose:
+#sudo docker build -t project1_rough:latest .
+#sudo docker run -it -d -p 5000:5000 --name p1_rough project1_rough
+
+# Run script with docker-compose:
+sudo docker-compose up -d
 
