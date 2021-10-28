406fb27a13b262523269fbda9e4583e4212dd2b9
Sofi <sofiv@uoregon.edu>
2021-10-27 22:09:43 -0700

    auth test

--- flask_app/auth/views.py
+++ flask_app/auth/views.py
@@ -1,13 +1,13 @@
 from . import auth_blueprint
-from flask import render_template, Flask, jsonify, request, current_app
+from flask import Flask, jsonify, render_template, request, current_app
 from . import db
 
 mongo = db.Mongodb()
 mongo.connect()
 
-@auth_blueprint.route('/register', methods=['GET'])
+@auth_blueprint.route('/registration', methods=['GET'])
 def register():
-    return render_template('register.html') 
+    return render_template('registration.html')
 
 # create a connection/route between URL /login and the function login() that returns a response
 @auth_blueprint.route('/login', methods=['GET'])
