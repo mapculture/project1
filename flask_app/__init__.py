a3a3f79bfae28a6314e28331dc6ba936474d8fb9
Kelemen Szimonisz <kelemen.szimonisz@gmail.com>
2021-10-27 20:52:53 -0700

    about module fix

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -39,6 +39,7 @@ def create_app(test_config=None):
     with app.app_context():
         # import blueprints
         from .home import home_blueprint
+        from .about import about_blueprint
         from .getmap import getmap_blueprint 
         from .testmap import testmap_blueprint
         from .testmapOOP import testmapOOP_blueprint
@@ -50,6 +51,7 @@ def create_app(test_config=None):
 
         # register blueprints
         app.register_blueprint(home_blueprint)
+        app.register_blueprint(about_blueprint)
         app.register_blueprint(getmap_blueprint)
         app.register_blueprint(testmap_blueprint)
         app.register_blueprint(testmapOOP_blueprint)
