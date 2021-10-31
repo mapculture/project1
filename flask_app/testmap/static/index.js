8cf6008ac9fd1cbbf25e5eb2f9f1f76d10acb00c
Kelemen Szimonisz <kelemen.szimonisz@gmail.com>
2021-10-30 18:16:57 -0700

    implemented algorithm help button and message showing/hiding

--- flask_app/testmap/static/index.js
+++ flask_app/testmap/static/index.js
@@ -609,4 +609,10 @@ document.addEventListener("DOMContentLoaded", function() {
             destEntries[i].value = "";
         }
     });
+        document.getElementById('help-bttn').addEventListener('click', (e) => {
+            submitButton.style.display = "show";
+            header.style.color="lightgreen";
+            header.style.textDecoration = "none";
+            header.innerText= "Choosing an algorithm:\n\nIf you can wait, the Genetic algorithm will get you a close-to-optimum route.\n\nIf you're in a hurry, the Prim’s algorithm will get you an efficient route fast.";
+    });
 });
